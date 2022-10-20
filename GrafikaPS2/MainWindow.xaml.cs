836e4b6c1e4a2ecf6e4438a5ad24ccb1b58ce09f
pawjo <pawima00@gmail.com>
2022-10-20 20:28:19 +0200

    Added PS2 project

---
+++ GrafikaPS2/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Win32;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace GrafikaPS2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            var openJPEGDialong = new OpenFileDialog();
+
+            openJPEGDialong.Filter = "Image files(*.ppm)|*.ppm";
+            openJPEGDialong.Title = "Open an Image File";
+
+            if (openJPEGDialong.ShowDialog() == true)
+            {
+
+                MainImage.Source = ToBitmapImage(ReadBitmapFromPPM(openJPEGDialong));
+            }
+        }
+
+        private Bitmap ReadBitmapFromPPM(OpenFileDialog openJPEGDialong)
+        {
+            var ppm = new PPM(openJPEGDialong);
+
+            MainImage.Width = ppm.Width;
+            MainImage.Height = ppm.Height;
+
+            //if (ppm.Width < 100)
+            //{
+            //    MainImage.Width = 20 * ppm.Width;
+            //    MainImage.Height = 20 * ppm.Width;
+            //}
+
+            //if (ppm.Width > 1000)
+            //{
+            //    MainImage.Width = ppm.Width / 10;
+            //    MainImage.Height = ppm.Width / 10;
+            //}
+
+            return ppm.Bitmap;
+        }
+
+        public BitmapImage ToBitmapImage(Bitmap bitmap)
+        {
+            using (var memory = new MemoryStream())
+            {
+                bitmap.Save(memory, ImageFormat.Bmp);
+                memory.Position = 0;
+
+                var bitmapImage = new BitmapImage();
+
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+
+                return bitmapImage;
+            }
+        }
+    }
+}
---
+++ GrafikaPS2/RGB.cs
@@ -0,0 +1,9 @@
+﻿namespace GrafikaPS2
+{
+    public class RGB
+    {
+        public int R { get; set; } = 0;
+        public int G { get; set; } = 0;
+        public int B { get; set; } = 0;
+    }
+}
