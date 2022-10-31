fbc730bf48f9060799ef7d21ec9c64b3f0f98177
pawjo <pawima00@gmail.com>
2022-10-31 01:57:43 +0100

    Added cmyk and hsv converter

--- GrafikaPS3/JS/script.js
+++ GrafikaPS3/JS/script.js
@@ -17,6 +17,8 @@ var faceIndices = ['a', 'b', 'c'];
 var vertexIndex;
 var point;
 
+const activeClassName = "active-panel";
+
 geom.faces.forEach(function (face) {
     for (var i = 0; i < 3; i++) {
 
@@ -47,13 +49,22 @@ function render() {
     renderer.render(scene, camera);
 };
 
+// function showElement(id) {
+//     const panels = document.getElementsByClassName("panel");
+//     for (let element of panels) {
+//         element.style.display = "none";
+//     }
+//     const element = document.getElementById(id);
+//     element.style.display = "grid";
+// }
+
 function showElement(id) {
-    const panels = document.getElementsByClassName("panel");
-    for (let element of panels) {
-        element.style.display = "none";
-    }
+    const activePanel = document.getElementsByClassName(activeClassName)[0];
+    activePanel.style.display = "none";
+    activePanel.classList.remove(activeClassName);
     const element = document.getElementById(id);
     element.style.display = "grid";
+    element.classList.add(activeClassName);
 }
 
 // function onVaueChange(e,inputToUpdateId){
@@ -63,14 +74,21 @@ function showElement(id) {
 
 function onValueChange(e) {
     const target = e.target;
-    if (target.value < 0) {
-        target.value = 0;
+    checkAndFixValue(target);
+
+    const activePanel = document.getElementsByClassName(activeClassName)[0];
+
+    switch (activePanel.id) {
+        case "rgb-panel":
+            setColorFromRGB();
+            break;
+        case "cmyk-panel":
+            setColorFromCMYK();
+            break;
+        case "hsv-panel":
+            setColorFromHSV();
+            break;
     }
-
-    checkSibling(target.nextElementSibling, target.value);
-    checkSibling(target.previousElementSibling, target.value);
-
-    setColorFromRGB();
 }
 
 function checkSibling(sibling, value) {
@@ -81,15 +99,50 @@ function checkSibling(sibling, value) {
     }
 }
 
-function addListeners() {
-    const colorInputs = document.getElementsByClassName("color-input");
+function addListeners(controlClassName, max) {
+    const colorInputs = document.getElementsByClassName(controlClassName);
 
     for (let input of colorInputs) {
-        input.setAttribute("min", "1");
-        input.setAttribute("max", "255");
+        input.setAttribute("min", "0");
+        input.setAttribute("max", max);
         input.setAttribute("value", "0");
         input.addEventListener("change", onValueChange);
     }
+}
+
+function getAndFixValue(id, min, max) {
+    const control = document.getElementById(id);
+
+    if (!control) {
+        return NaN;
+    }
+
+    if (control.value < min) {
+        control.value = min;
+    }
+    else if (control.value > max) {
+        control.value = max;
+    }
+
+    checkSibling(control.nextElementSibling, control.value);
+    checkSibling(control.previousElementSibling, control.value);
+
+    return control.value;
+}
+
+function checkAndFixValue(control) {
+    const min = parseInt(control.getAttribute("min"));
+    const max = parseInt(control.getAttribute("max"));
+
+    if (control.value < min) {
+        control.value = min;
+    }
+    else if (control.value > max) {
+        control.value = max;
+    }
+
+    checkSibling(control.nextElementSibling, control.value);
+    checkSibling(control.previousElementSibling, control.value);
 }
 
 function getRGBValues() {
@@ -100,43 +153,38 @@ function getRGBValues() {
     return { r: r, g: g, b: b };
 }
 
-function setColorFromRGB() {
-    const values = getRGBValues();
-    document.getElementById("rgb-color-panel").style.backgroundColor = `rgb(${values.r},${values.g},${values.b})`;
-
-    const k = 1 - (Math.max(values.r, values.g, values.b) / 255);
-    const c = (1 - values.r / 255 - k) / (1 - k);
-    const m = (1 - values.g / 255 - k) / (1 - k);
-    const y = (1 - values.b / 255 - k) / (1 - k);
-
-    document.getElementById("rgb-cmyk-c").value = c;
-    document.getElementById("rgb-cmyk-m").value = m;
-    document.getElementById("rgb-cmyk-y").value = y;
-    document.getElementById("rgb-cmyk-k").value = k;
-
-    setCMYKFromRGB(values);
-    setHSVFromRGB(values);
+function setOutput(id, value) {
+    const control = document.getElementById(id);
+    control.value = Math.round(value);
 }
 
-function setCMYKFromRGB(values) {
-    const k = 1 - (Math.max(values.r, values.g, values.b) / 255);
-    const c = (1 - values.r / 255 - k) / (1 - k);
-    const m = (1 - values.g / 255 - k) / (1 - k);
-    const y = (1 - values.b / 255 - k) / (1 - k);
 
-    document.getElementById("rgb-cmyk-c").value = c;
-    document.getElementById("rgb-cmyk-m").value = m;
-    document.getElementById("rgb-cmyk-y").value = y;
-    document.getElementById("rgb-cmyk-k").value = k;
+function setCMYKOutput(values, idPrefix) {
+    const k = 1 - (Math.max(values.r, values.g, values.b) / 255);
+    let c = 0;
+    let m = 0;
+    let y = 0;
+
+    if (k < 1) {
+        c = (1 - values.r / 255 - k) / (1 - k) * 100;
+        m = (1 - values.g / 255 - k) / (1 - k) * 100;
+        y = (1 - values.b / 255 - k) / (1 - k) * 100;
+    }
+
+    setOutput(idPrefix + "-cmyk-c", c);
+    setOutput(idPrefix + "-cmyk-m", m);
+    setOutput(idPrefix + "-cmyk-y", y);
+    setOutput(idPrefix + "-cmyk-k", k * 100);
 }
-function setHSVFromRGB(values) {
+
+function setHSVOutput(values, idPrefix) {
     const r = values.r / 255.0;
     const g = values.g / 255.0;
     const b = values.b / 255.0;
 
-    const cmax = Math.max(r, Math.max(g, b)); // maximum of r, g, b
-    const cmin = Math.min(r, Math.min(g, b)); // minimum of r, g, b
-    const diff = cmax - cmin; // diff of cmax and cmin.
+    const cmax = Math.max(r, Math.max(g, b));
+    const cmin = Math.min(r, Math.min(g, b));
+    const diff = cmax - cmin;
     const s = cmax === 0 ? 0 : diff / cmax * 100;
     const v = cmax * 100;
     let h = -1;
@@ -156,11 +204,125 @@ function setHSVFromRGB(values) {
             break;
     }
 
-    document.getElementById("rgb-hsv-h").value = h;
-    document.getElementById("rgb-hsv-s").value = s;
-    document.getElementById("rgb-hsv-v").value = v;
+    setOutput(idPrefix + "-hsv-h", h);
+    setOutput(idPrefix + "-hsv-s", s);
+    setOutput(idPrefix + "-hsv-v", v);
 }
 
+function setRGBOutput(values, idPrefix) {
+    setOutput(idPrefix + "-rgb-r", values.r);
+    setOutput(idPrefix + "-rgb-g", values.g);
+    setOutput(idPrefix + "-rgb-b", values.b);
+}
+
+function setColorPanel(values, idPrefix) {
+    const panel = document.getElementById(idPrefix + "-color-panel");
+    panel.style.backgroundColor = `rgb(${values.r},${values.g},${values.b})`;
+}
+
+function setColorFromRGB() {
+    const values = getRGBValues();
+    setColorPanel(values, "rgb");
+
+    // const k = 1 - (Math.max(values.r, values.g, values.b) / 255);
+    // const c = (1 - values.r / 255 - k) / (1 - k);
+    // const m = (1 - values.g / 255 - k) / (1 - k);
+    // const y = (1 - values.b / 255 - k) / (1 - k);
+
+    // document.getElementById("rgb-cmyk-c").value = c;
+    // document.getElementById("rgb-cmyk-m").value = m;
+    // document.getElementById("rgb-cmyk-y").value = y;
+    // document.getElementById("rgb-cmyk-k").value = k;
+
+    setCMYKOutput(values, "rgb");
+    setHSVOutput(values, "rgb");
+}
+
+function getRGBFromCMYK(c, m, y, k) {
+    const kDiff = 1 - k / 100;
+    const r = 255 * (1 - c / 100) * kDiff;
+    const g = 255 * (1 - m / 100) * kDiff;
+    const b = 255 * (1 - y / 100) * kDiff;
+    return { r: r, g: g, b: b };
+}
+
+function getRGBFromHSV(h, s, v) {
+    const normalizedH = h == 360 ? 0 : h / 60;
+    const fract = normalizedH - Math.floor(normalizedH);
+    const roundedH = Math.round(normalizedH);
+    const normalizedS = s / 100;
+    const normalizedV = v / 100;
+    const p = normalizedV * (1 - normalizedS);
+    const q = normalizedV * (1 - (normalizedS * fract));
+    const t = normalizedV * (1 - (normalizedS * (1 - fract)));
+
+    let r = 0, g = 0, b = 0;
+
+    switch (roundedH) {
+        case 0:
+            r = normalizedV;
+            g = t;
+            b = p;
+            break;
+        case 1:
+            r = q;
+            g = normalizedV;
+            b = p;
+            break;
+        case 2:
+            r = p;
+            g = normalizedV;
+            b = t;
+            break;
+
+        case 3:
+            r = p;
+            g = q;
+            b = normalizedV;
+            break;
+        case 4:
+            r = t;
+            g = p;
+            b = normalizedV;
+            break;
+        case 5:
+            r = normalizedV;
+            g = p;
+            b = q;
+            break;
+    }
+
+    const result = { r: r * 255, g: g * 255, b: b * 255 };
+    return result;
+}
+
+function setColorFromCMYK() {
+    const c = document.getElementById("cmyk-input-c-field").value;
+    const m = document.getElementById("cmyk-input-m-field").value;
+    const y = document.getElementById("cmyk-input-y-field").value;
+    const k = document.getElementById("cmyk-input-k-field").value;
+
+    const rgbValues = getRGBFromCMYK(c, m, y, k);
+    setColorPanel(rgbValues, "cmyk");
+    setHSVOutput(rgbValues, "cmyk");
+    setRGBOutput(rgbValues, "cmyk");
+}
+
+function setColorFromHSV() {
+    const h = document.getElementById("hsv-input-h-field").value;
+    const s = document.getElementById("hsv-input-s-field").value;
+    const v = document.getElementById("hsv-input-v-field").value;
+
+    const rgbValues = getRGBFromHSV(h, s, v);
+    setColorPanel(rgbValues, "hsv");
+    setRGBOutput(rgbValues, "hsv");
+    setCMYKOutput(rgbValues, "hsv");
+}
 
 render();
-addListeners();
+addListeners("rgb-color-input", 255);
+addListeners("percent-color-input", 100);
+addListeners("degres-color-input", 360);
+setColorFromRGB();
+setColorFromCMYK();
+setColorFromHSV();
