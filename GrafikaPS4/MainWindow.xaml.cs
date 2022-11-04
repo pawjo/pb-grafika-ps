edb4a2bb50de281c9315df4060fcd4372af36f5c
pawjo <pawima00@gmail.com>
2022-11-05 00:31:53 +0100

    Added point transforms without grey scale 2

--- GrafikaPS4/MainWindow.xaml.cs
+++ GrafikaPS4/MainWindow.xaml.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.Win32;
+﻿using Accessibility;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Interop;
@@ -21,13 +23,15 @@ namespace GrafikaPS4
         private System.Windows.Point _origin;  // Original Offset of MainImage
         private System.Windows.Point _start;   // Original Position of the mouse
 
-        private WriteableBitmap _bitmap;
+        private WriteableBitmap _writeableBitmap;
 
         private int _currentImageFormat = 0;
 
         private string _windowTitle = "Netpbm Viewer PTPW";
 
         public bool IsLoading { get; set; }
+
+        public bool IsImageLoaded { get => _writeableBitmap != null; }
 
         public MainWindow()
         {
@@ -45,7 +49,7 @@ namespace GrafikaPS4
             var openFileDialog = new OpenFileDialog();
 
             openFileDialog.Title = "Open an image file";
-            openFileDialog.Filter = "BMP|*.bmp|GIF|*.gif|JPG|*.jpg;*.jpeg|PNG|*.png|TIFF|*.tif;*.tiff|" +
+            openFileDialog.Filter = "JPG|*.jpg;*.jpeg|PNG|*.png|BMP|*.bmp|GIF|*.gif|TIFF|*.tif;*.tiff|" +
                 "All Graphics Types|*.bmp;*.jpg;*.jpeg;*.png;*.tif;*.tiff";
 
 
@@ -54,55 +58,14 @@ namespace GrafikaPS4
                 try
                 {
                     var bitmapImage = new BitmapImage(new System.Uri(openFileDialog.FileName));
-                    _bitmap = new WriteableBitmap(bitmapImage);
-                    MainImage.Source = _bitmap;
+                    _writeableBitmap = new WriteableBitmap(bitmapImage);
+                    MainImage.Source = _writeableBitmap;
                 }
                 catch
                 {
                     MessageBox.Show("Open file error");
                 }
             }
-        }
-
-        private void ReadFile(OpenFileDialog openFileDialog)
-        {
-            //using (var ppm = new NetpbmReader(openFileDialog))
-            //{
-
-            //    if (!ppm.ReadFile())
-            //    {
-            //        MessageBox.Show("Open file error");
-            //        _bitmap = null;
-            //    }
-
-            //    MainImage.RenderTransform = new MatrixTransform();
-
-            //    if (ppm.Width / ppm.Height > ImageStackPanel.ActualWidth / ImageStackPanel.ActualHeight)
-            //    {
-            //        MainImage.Width = ImageStackPanel.ActualWidth;
-            //    }
-            //    else
-            //    {
-            //        MainImage.Height = ImageStackPanel.ActualHeight;
-            //    }
-
-            //    if (ppm.Comments.Count == 0)
-            //    {
-            //        CommentsListBox.ItemsSource = new List<string>() { "No comments" };
-
-            //    }
-            //    else
-            //    {
-            //        CommentsListBox.ItemsSource = ppm.Comments;
-            //    }
-
-            //    int format = (ppm.Format[1] - 48) % 3;
-            //    _currentImageFormat = format == 0 ? 3 : format;
-
-            //    MainViewerWindow.Title = $"{_windowTitle} - {openFileDialog.FileName}";
-
-            //    _bitmap = ppm.Bitmap;
-            //}
         }
 
         public BitmapImage GetBitmapImage(Bitmap bitmap)
@@ -132,8 +95,6 @@ namespace GrafikaPS4
             _origin.X = MainImage.RenderTransform.Value.OffsetX;
             _origin.Y = MainImage.RenderTransform.Value.OffsetY;
         }
-
-
 
         private void MainImage_MouseMove(object sender, MouseEventArgs e)
         {
@@ -165,32 +126,71 @@ namespace GrafikaPS4
             MainImage.ReleaseMouseCapture();
         }
 
-        private Bitmap GetWritableBitmap()
+        private Bitmap GetBitmapFromWritableBitmap()
         {
-            if (_bitmap == null)
+            if (_writeableBitmap == null)
             {
                 return null;
             }
 
-            using (MemoryStream outStream = new MemoryStream())
+            using (var outStream = new MemoryStream())
             {
-                BitmapEncoder enc = new BmpBitmapEncoder();
-                enc.Frames.Add(BitmapFrame.Create(_bitmap));
+                var enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(_writeableBitmap));
                 enc.Save(outStream);
                 var bmp = new Bitmap(outStream);
                 return bmp;
             }
         }
 
+        private Bitmap GetBitmapFromWritableBitmap(WriteableBitmap writeableBitmap)
+        {
+            if (writeableBitmap == null)
+            {
+                return null;
+            }
+
+            using (var outStream = new MemoryStream())
+            {
+                var enc = new BmpBitmapEncoder();
+                enc.Frames.Add(BitmapFrame.Create(writeableBitmap));
+                enc.Save(outStream);
+                var bmp = new Bitmap(outStream);
+                return bmp;
+            }
+        }
+
+        private void SetNewWriteableBitmap(Bitmap bitmap)
+        {
+            BitmapSource bitmapSource = Imaging.CreateBitmapSourceFromHBitmap(bitmap.GetHbitmap(),
+                IntPtr.Zero,
+                Int32Rect.Empty,
+                BitmapSizeOptions.FromEmptyOptions());
+
+            _writeableBitmap = new WriteableBitmap(bitmapSource);
+            MainImage.Source = _writeableBitmap;
+        }
+
+        private WriteableBitmap GetWriteableBitmapFormBitmap(Bitmap bitmap)
+        {
+            BitmapSource bitmapSource = Imaging.CreateBitmapSourceFromHBitmap(bitmap.GetHbitmap(),
+                IntPtr.Zero,
+                Int32Rect.Empty,
+                BitmapSizeOptions.FromEmptyOptions());
+
+            var writeableBitmap = new WriteableBitmap(bitmapSource);
+            return writeableBitmap;
+        }
+
         private void GrayScale(object sender, RoutedEventArgs e)
         {
-            //if (EditableImage == null)
-            //{
-            //    MessageBox.Show("Podano złą wartość");
-            //    return;
-            //}
+            if (_writeableBitmap == null)
+            {
+                MessageBox.Show("Brak obrazu");
+                return;
+            }
 
-            var bitmap = GetWritableBitmap();
+            var bitmap = GetBitmapFromWritableBitmap();
 
             for (int i = 0; i < bitmap.Width; i++)
             {
@@ -208,15 +208,256 @@ namespace GrafikaPS4
             SetNewWriteableBitmap(bitmap);
         }
 
-        private void SetNewWriteableBitmap(Bitmap bitmap)
+        private void test1(object sender, RoutedEventArgs e)
         {
-            BitmapSource bitmapSource = Imaging.CreateBitmapSourceFromHBitmap(bitmap.GetHbitmap(),
-                IntPtr.Zero,
-                Int32Rect.Empty,
-                BitmapSizeOptions.FromEmptyOptions());
+            Loading.IsBusy = true;
+        }
 
-            _bitmap = new WriteableBitmap(bitmapSource);
-            MainImage.Source = _bitmap;
+
+        private async void Add(object sender, RoutedEventArgs e)
+        {
+            int value = GetPointTransformValue();
+            Loading.IsBusy = true;
+
+            var bitmap = GetBitmapFromWritableBitmap();
+
+            bitmap = await Task.Run(() =>
+            {
+
+                for (int i = 0; i < bitmap.Width; i++)
+                {
+                    for (int j = 0; j < bitmap.Height; j++)
+                    {
+                        var color = bitmap.GetPixel(i, j);
+
+                        var r = color.R + value;
+                        var g = color.G + value;
+                        var b = color.B + value;
+
+                        if (r > 255)
+                            r = 255;
+
+                        if (g > 255)
+                            g = 255;
+
+                        if (b > 255)
+                            b = 255;
+
+                        bitmap.SetPixel(i, j, System.Drawing.Color.FromArgb(color.A, r, g, b));
+                    }
+                }
+
+                return bitmap;
+            });
+
+            SetNewWriteableBitmap(bitmap);
+            Loading.IsBusy = false;
+        }
+
+        private async void Brighter(object sender, RoutedEventArgs e)
+        {
+            int value = GetPointTransformValue();
+            Loading.IsBusy = true;
+
+            var bitmap = GetBitmapFromWritableBitmap();
+
+            bitmap = await Task.Run(() =>
+            {
+                var lut = new int[256];
+
+                for (int i = 0; i < 256; i++)
+                {
+                    lut[i] = i + value;
+
+                    if (lut[i] > 255)
+                    {
+                        lut[i] = 255;
+                    }
+                }
+
+                bitmap = SetBitmapFromLut(bitmap, lut);
+
+                return bitmap;
+            });
+
+            SetNewWriteableBitmap(bitmap);
+            Loading.IsBusy = false;
+        }
+
+        private async void Darker(object sender, RoutedEventArgs e)
+        {
+            int value = GetPointTransformValue();
+            Loading.IsBusy = true;
+
+            var bitmap = GetBitmapFromWritableBitmap();
+
+            bitmap = await Task.Run(() =>
+            {
+                var lut = new int[256];
+
+                for (int i = 0; i < 256; i++)
+                {
+                    lut[i] = i - value;
+
+                    if (lut[i] < 0)
+                    {
+                        lut[i] = 0;
+                    }
+                }
+
+                bitmap = SetBitmapFromLut(bitmap, lut);
+
+                return bitmap;
+            });
+
+            SetNewWriteableBitmap(bitmap);
+            Loading.IsBusy = false;
+        }
+
+        private Bitmap SetBitmapFromLut(Bitmap bitmap, int[] lut)
+        {
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                for (int j = 0; j < bitmap.Height; j++)
+                {
+                    var color = bitmap.GetPixel(i, j);
+                    var r = lut[color.R];
+                    var g = lut[color.G];
+                    var b = lut[color.B];
+
+                    bitmap.SetPixel(i, j, System.Drawing.Color.FromArgb(color.A, r, g, b));
+                }
+            }
+
+            return bitmap;
+        }
+
+        private async void Subtract(object sender, RoutedEventArgs e)
+        {
+            int value = GetPointTransformValue();
+            Loading.IsBusy = true;
+
+            var bitmap = GetBitmapFromWritableBitmap();
+
+            bitmap = await Task.Run(() =>
+            {
+
+                for (int i = 0; i < bitmap.Width; i++)
+                {
+                    for (int j = 0; j < bitmap.Height; j++)
+                    {
+                        var color = bitmap.GetPixel(i, j);
+
+                        var r = color.R - value;
+                        var g = color.G - value;
+                        var b = color.B - value;
+
+                        if (r < 0)
+                            r = 0;
+
+                        if (g < 0)
+                            g = 0;
+
+                        if (b < 0)
+                            b = 0;
+
+                        bitmap.SetPixel(i, j, System.Drawing.Color.FromArgb(color.A, r, g, b));
+                    }
+                }
+
+                return bitmap;
+            });
+
+            SetNewWriteableBitmap(bitmap);
+            Loading.IsBusy = false;
+        }
+
+        private async void Multiply(object sender, RoutedEventArgs e)
+        {
+            int value = GetPointTransformValue();
+            Loading.IsBusy = true;
+
+            var bitmap = GetBitmapFromWritableBitmap();
+
+            bitmap = await Task.Run(() =>
+            {
+
+                for (int i = 0; i < bitmap.Width; i++)
+                {
+                    for (int j = 0; j < bitmap.Height; j++)
+                    {
+                        var color = bitmap.GetPixel(i, j);
+
+                        var r = color.R * value;
+                        var g = color.G * value;
+                        var b = color.B * value;
+
+                        if (r > 255)
+                            r = 255;
+
+                        if (g > 255)
+                            g = 255;
+
+                        if (b > 255)
+                            b = 255;
+
+                        bitmap.SetPixel(i, j, System.Drawing.Color.FromArgb(color.A, r, g, b));
+                    }
+                }
+
+                return bitmap;
+            });
+
+            SetNewWriteableBitmap(bitmap);
+            Loading.IsBusy = false;
+        }
+
+        private async void Divide(object sender, RoutedEventArgs e)
+        {
+            int value = GetPointTransformValue();
+            Loading.IsBusy = true;
+
+            var bitmap = GetBitmapFromWritableBitmap();
+
+            bitmap = await Task.Run(() =>
+            {
+
+                for (int i = 0; i < bitmap.Width; i++)
+                {
+                    for (int j = 0; j < bitmap.Height; j++)
+                    {
+                        var color = bitmap.GetPixel(i, j);
+
+                        var r = color.R / value;
+                        var g = color.G / value;
+                        var b = color.B / value;
+
+                        bitmap.SetPixel(i, j, System.Drawing.Color.FromArgb(color.A, r, g, b));
+                    }
+                }
+
+                return bitmap;
+            });
+
+            SetNewWriteableBitmap(bitmap);
+            Loading.IsBusy = false;
+        }
+
+        private int GetPointTransformValue()
+        {
+            if (_writeableBitmap == null)
+            {
+                MessageBox.Show("Not loaded image");
+                return 0;
+            }
+
+            if (int.TryParse(PointTransformsValue.Text, out int value) && value > 0)
+            {
+                return value;
+            }
+
+            MessageBox.Show("Wrong value");
+            return 0;
         }
     }
 }
